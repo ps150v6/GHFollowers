e4930d75b8fa499b7b0057abb9f18608c03f9801
Matt Rodriguez <matthew.b.rodriguez@gmail.com>
2025-01-06 20:34:45 -0800

    Planning our Network Calls - API, JSON, Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Matthew Rodriguez on 1/6/25.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
