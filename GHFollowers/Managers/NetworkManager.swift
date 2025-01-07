af941d290ffc0b89ee7d059949f0f3e1225d22db
Matt Rodriguez <matthew.b.rodriguez@gmail.com>
2025-01-06 22:52:42 -0800

    Refactor Error Messages

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -15,39 +15,30 @@ class NetworkManager {
 
     func getFollowers(
         for username: String, page: Int,
-        completion: @escaping ([Follower]?, String?) -> Void
+        completion: @escaping ([Follower]?, ErrorMessage?) -> Void
     ) {
         let endpoint =
             "\(baseURL)/\(username)/followers?per_page=100&page=\(page)"
 
         guard let url = URL(string: endpoint) else {
-            completion(
-                nil,
-                "This username created an invalid request. Please try again.")
+            completion(nil, .invalidUsername)
             return
         }
 
         let task = URLSession.shared.dataTask(with: url) {
             data, response, error in
             if error != nil {
-                completion(
-                    nil,
-                    "Unable to complete your request. Please check your internet connection."
-                )
+                completion(nil, .unableToComplete)
                 return
             }
             guard let response = response as? HTTPURLResponse,
                 response.statusCode == 200
             else {
-                completion(
-                    nil, "Invalid response from the server. Please try again.")
+                completion(nil, .invalidResponse)
                 return
             }
             guard let data = data else {
-                completion(
-                    nil,
-                    "The data received from the server was invalid. Please try again."
-                )
+                completion(nil, .invalidData)
                 return
             }
 
@@ -57,10 +48,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completion(followers, nil)
             } catch {
-                completion(
-                    nil,
-                    "The data received from the server was invalid. Please try again."
-                )
+                completion(nil, .invalidData)
             }
         }
 
--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -14,13 +14,16 @@ class FollowerListVC: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
-        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] followers, errorMessage in
+
+        NetworkManager.shared.getFollowers(for: username, page: 1) {
+            [weak self] followers, errorMessage in
             guard let followers = followers else {
-                self?.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage ?? "", buttonTitle: "OK")
+                self?.presentGFAlertOnMainThread(
+                    title: "Bad Stuff Happened",
+                    message: errorMessage?.rawValue ?? "", buttonTitle: "OK")
                 return
             }
-            
+
             print("Followers.count = \(followers.count)")
             print("Followers = \(followers)")
         }
